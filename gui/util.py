5bae78eb9b43d9c4f1a2b79ab3d2d709289f81dc
Timothy Andrew <mail@timothyandrew.net>
2017-03-30 23:19:47 +0530

    remove logging code for windows

--- gui/util.py
+++ gui/util.py
@@ -1,3 +1,4 @@
 def log(message):
-    with open("/tmp/log.txt", "a") as text_file:
-        text_file.write(message + "\n")
+    # with open("/tmp/log.txt", "a") as text_file:
+    #     text_file.write(message + "\n")
+    pass
